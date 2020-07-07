3314a8f0ef592815345b2fa0ab339150a51f5ee9
lukas1us <lukas1us@users.noreply.github.com>
2020-07-07 10:54:05 +0200

    Updates

---
+++ precache-manifest.e88fb30729edb06b096a6142990035a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad08e406b4328ef8f850b51ee0703e26",
+    "url": "/moviedb_app/index.html"
+  },
+  {
+    "revision": "d1f1530f9a3450846735",
+    "url": "/moviedb_app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5939153665bcb5ed2e1b",
+    "url": "/moviedb_app/static/css/main.2c2e70f9.chunk.css"
+  },
+  {
+    "revision": "d1f1530f9a3450846735",
+    "url": "/moviedb_app/static/js/2.16c8cce3.chunk.js"
+  },
+  {
+    "revision": "ae96ce37367c3b15926eb6b17d3aa94e",
+    "url": "/moviedb_app/static/js/2.16c8cce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5939153665bcb5ed2e1b",
+    "url": "/moviedb_app/static/js/main.42a562f8.chunk.js"
+  },
+  {
+    "revision": "0e60b3dd784bd2f1f67d",
+    "url": "/moviedb_app/static/js/runtime-main.b271cf94.js"
+  }
+]);
